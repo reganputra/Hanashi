2fae88694161cddf7e76de81c0a3dec1f34a9a1c
reganputra <ramadhanregan334@gmail.com>
2024-11-03 10:03:32 +0700

    feat: add userpreference

--- app/src/main/java/com/example/sutoriapuri/data/userpref/UserPref.kt
+++ app/src/main/java/com/example/sutoriapuri/data/userpref/UserPref.kt
@@ -1,4 +1,60 @@
 package com.example.sutoriapuri.data.userpref
 
-class UserPref {
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import com.example.sutoriapuri.data.model.UserModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "session")
+
+class UserPreference private constructor(private val dataStore: DataStore<Preferences>) {
+
+    suspend fun saveSession(user: UserModel) {
+        dataStore.edit {
+            it[USER_ID] = user.userId
+            it[NAME_KEY] = user.name
+            it[TOKEN_KEY] = user.token
+        }
+    }
+
+    fun getSession(): Flow<UserModel> {
+        return dataStore.data.map {
+            UserModel(
+                it[USER_ID] ?: "",
+                it[NAME_KEY] ?: "",
+                it[TOKEN_KEY] ?: ""
+            )
+        }
+    }
+
+    suspend fun logout() {
+        dataStore.edit { preferences ->
+            preferences.clear()
+        }
+    }
+
+    companion object {
+        @Volatile
+        private var INSTANCE: UserPreference? = null
+
+        private val USER_ID = stringPreferencesKey("userId")
+        private val NAME_KEY = stringPreferencesKey("name")
+        private val TOKEN_KEY = stringPreferencesKey("token")
+
+
+        fun getInstance(dataStore: DataStore<Preferences>): UserPreference {
+            return INSTANCE ?: synchronized(this) {
+                val instance = UserPreference(dataStore)
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
 }
