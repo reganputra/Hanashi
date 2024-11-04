c86e9b5e657b351b32a37fbfce8f36393017bd27
reganputra <ramadhanregan334@gmail.com>
2024-11-04 08:13:50 +0700

    feat: add viewmodel factory

--- app/src/main/java/com/example/sutoriapuri/data/ViewModelFactory.kt
+++ app/src/main/java/com/example/sutoriapuri/data/ViewModelFactory.kt
@@ -1,4 +1,47 @@
 package com.example.sutoriapuri.data
 
-class ViewModelFactory {
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.sutoriapuri.ui.MainViewModel
+import com.example.sutoriapuri.ui.login.LoginViewModel
+import com.example.sutoriapuri.ui.register.RegisterViewModel
+
+class ViewModelFactory(private val storyRepository: StoryRepository):
+    ViewModelProvider.NewInstanceFactory(){
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T{
+        return when{
+            modelClass.isAssignableFrom(MainViewModel::class.java) -> {
+                MainViewModel(storyRepository) as T
+            }
+
+            modelClass.isAssignableFrom(LoginViewModel::class.java) -> {
+                LoginViewModel(storyRepository) as T
+            }
+
+            modelClass.isAssignableFrom(RegisterViewModel::class.java) -> {
+                RegisterViewModel(storyRepository) as T
+            }
+
+
+            else -> {
+                throw IllegalArgumentException("Unknown ViewModel class: " + modelClass.name)
+            }
+        }
+    }
+
+    companion object{
+        @Volatile
+        private var INSTANCE: ViewModelFactory? = null
+        @JvmStatic
+        fun getInstance(context: Context): ViewModelFactory {
+            if (INSTANCE == null) {
+                synchronized(ViewModelFactory::class.java) {
+                    INSTANCE = ViewModelFactory(Injection.provideRepository(context))
+                }
+            }
+            return INSTANCE as ViewModelFactory
+        }
+    }
 }
