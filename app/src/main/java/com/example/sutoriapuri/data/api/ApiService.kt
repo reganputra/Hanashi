2f2e8099addd5c7b8cf0bef89b13385b547195ed
reganputra <ramadhanregan334@gmail.com>
2024-11-05 08:02:01 +0700

    feat: add get story method

--- app/src/main/java/com/example/sutoriapuri/data/api/ApiService.kt
+++ app/src/main/java/com/example/sutoriapuri/data/api/ApiService.kt
@@ -1,9 +1,11 @@
 package com.example.sutoriapuri.data.api
 
+import com.example.sutoriapuri.data.response.ListStoryResponse
 import com.example.sutoriapuri.data.response.LoginResponse
 import com.example.sutoriapuri.data.response.RegisterResponse
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface ApiService {
@@ -21,5 +23,9 @@ interface ApiService {
         @Field("email") email: String,
         @Field("password") password: String
     ): LoginResponse
+
+
+    @GET("stories")
+    suspend fun getStories(): ListStoryResponse
 
 }
