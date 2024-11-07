afb8588a27dda213010eddfbbb1a3adc7eb73f8f
reganputra <ramadhanregan334@gmail.com>
2024-11-07 13:34:48 +0700

    feat: add upload endpoint

--- app/src/main/java/com/example/sutoriapuri/data/api/ApiService.kt
+++ app/src/main/java/com/example/sutoriapuri/data/api/ApiService.kt
@@ -3,11 +3,16 @@ package com.example.sutoriapuri.data.api
 import com.example.sutoriapuri.data.response.ListStoryResponse
 import com.example.sutoriapuri.data.response.LoginResponse
 import com.example.sutoriapuri.data.response.RegisterResponse
+import com.example.sutoriapuri.data.response.UploadStoryResponse
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.Multipart
 import retrofit2.http.POST
+import retrofit2.http.Part
 import retrofit2.http.Path
 
 interface ApiService {
@@ -38,4 +43,11 @@ interface ApiService {
         @Path("id") id: String
     ): ListStoryResponse
 
+    @Multipart
+    @POST("stories")
+    suspend fun uploadStory(
+        @Part file: MultipartBody.Part,
+        @Part("description") description: RequestBody,
+        @Header("Authorization") token: String
+    ): UploadStoryResponse
 }
