389ec2d093271102a0a49561abc62bdc4574ba97
reganputra <ramadhanregan334@gmail.com>
2024-11-09 07:03:58 +0700

    refactor: remove log

--- app/src/main/java/com/example/sutoriapuri/data/Injection.kt
+++ app/src/main/java/com/example/sutoriapuri/data/Injection.kt
@@ -1,7 +1,6 @@
 package com.example.sutoriapuri.data
 
 import android.content.Context
-import android.util.Log
 import com.example.sutoriapuri.data.api.ApiConfig
 import com.example.sutoriapuri.data.userpref.UserPreference
 import com.example.sutoriapuri.data.userpref.dataStore
@@ -12,8 +11,7 @@ object Injection {
     fun provideRepository(context: Context): StoryRepository {
         val pref = UserPreference.getInstance(context.dataStore)
         val user = runBlocking { pref.getSession().first() }
-        Log.d("Injection", "Retrieved token: ${user.token}")
-        val apiService = ApiConfig.getApiService(user.token)
+        val apiService = ApiConfig.getApiService(user.tokenKey)
         return StoryRepository.getInstance(apiService, pref)
     }
 }
